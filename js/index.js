ab8f45a64b7477a266eb880b1a1d7393614d5a8c
ade4e <wolapark@gmail.com>
2018-09-21 13:59:16 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,60 @@
+$("document").ready(function () {
+  function showError(search) {
+    $("#userSearchResultsTable").append(
+      "<p>" +
+      "Unfortunetly, there is no results for" +
+      ' "' +
+      search +
+      '"' +
+      ".</p>"
+    );
+  }
+  $("#icon-search").click(function () {
+    $("#userSearchResultsTable").empty();
+    var keyword = $("#search").val();
+
+    $.ajax({
+      url:
+        "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=info&list=search&inprop=url&srsearch=" +
+        keyword,
+      dataType: "jsonp",
+      type: "POST",
+      success: function (response) {
+        console.log(response.query);
+
+        if (response.query.searchinfo.totalhits === 0) {
+          showError(keyword);
+        } else {
+          for (i = 0; i < 10; i++) {
+            let link =
+              "<a href = 'http://en.wikipedia.org/?curid=" +
+              response.query.search[i].pageid +
+              "' target='_blank'>";
+
+            $("#userSearchResultsTable").append(
+              link + "<div class='result'>" +
+              "<h5>" +
+              response.query.search[i].title +
+              "</h5>" +
+              response.query.search[i].snippet +
+              "<br><br>" + "</div>" +
+              "</a>"
+            );
+          }
+        }
+      },
+
+      error: function () {
+        alert("Error retrieving search results, please refresh the page");
+      }
+
+      // do something with data
+    });
+  });
+  $("#search").keypress(function (e) {
+    if (e.which == 13) {
+      //Enter key pressed
+      $("#icon-search").click(); //Trigger search button click event
+    }
+  });
+});
